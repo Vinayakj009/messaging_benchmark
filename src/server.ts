6ac88fecf459c830b03d347a0e384bb0e9f89959
Vinayak <vinayakj009@gmail.com>
2023-12-29 13:16:57 +0530

    core - added code to check for server type in the ts code.

--- src/server.ts
+++ src/server.ts
@@ -4,41 +4,56 @@ import { ServerTester } from "./server/serverTester";
 import { MqttClient, MqttServer } from "./server/implementations/mqtt";
 import { uWebSocketClient, uWebSocketServer } from "./server/implementations/uWebSocket";
 
-let protocol = 'http'
-let host = '127.0.0.1'
-const port = 1883
+const serverTypes: {
+    [key: string]: {
+        client: any,
+        server: any,
+        protocol: string,
+        host: string,
+        port: number
+    }
+} = {
+    socketio: {
+        client: SocketIoClient,
+        server: SocketIoServer,
+        protocol: "http",
+        host: "127.0.0.1",
+        port: 1883
+    },
+    uwebsocket: {
+        client: uWebSocketClient,
+        server: uWebSocketServer,
+        protocol: "http",
+        host: "127.0.0.1",
+        port: 1883
+    },
+    mqtt: {
+        client: MqttClient,
+        server: MqttServer,
+        protocol: "mqtt",
+        host: "mqtt-broker",
+        port: 1883
+    }
+}
 
 
 const runType = process.argv[2];
 const serverType = process.argv[3];
 
+const server = serverTypes[serverType];
+
+if (!server) {
+    const allowedServerTypes: string[] = [];
+    for (const key in serverTypes) {
+        allowedServerTypes.push(key);
+    }
+    throw `Unknown server type: ${serverType}, expected on of ${allowedServerTypes.join(" , ")}`;
+}
+
 const serverTester = new ServerTester((Printer: Printable) => {
-    switch (serverType) {
-        case "socketio":
-            return new SocketIoServer(Printer, host, port, protocol);
-        case "uwebsocket":
-            return new uWebSocketServer(Printer, host, port, protocol);
-        case "mqtt":
-            protocol = "mqtt";
-            host = "mqtt-broker";
-            return new MqttServer(Printer, host, port, protocol);
-        default:
-            throw new Error("no server type specified");
-    }
+    return new server.server(Printer, server.host, server.port, server.protocol);
 }, (Printer: Printable, shreOfInterest: string) => {
-    switch (serverType) {
-        case "socketio":
-            return new SocketIoClient(Printer, shreOfInterest, host, port, protocol);
-        case "uwebsocket":
-            return new uWebSocketClient(Printer, shreOfInterest, host, port, protocol);
-        case "mqtt":
-            protocol = "mqtt";
-            host = "mqtt-broker";
-            return new MqttClient(Printer, shreOfInterest, host, port, protocol);;
-        default:
-            throw new Error("no server type specified");
-    }
-    
+    return new server.client(Printer, shreOfInterest, server.host, server.port, server.protocol);
 },100
     );
 
