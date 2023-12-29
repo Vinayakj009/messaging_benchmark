ec797c29801f0c44e2157e7e5bc99519ad8fb57a
Vinayak <vinayakj009@gmail.com>
2023-12-29 12:33:23 +0530

    core - removed stomp from list

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 tsc src/*.ts
 script="$1"
 
-if [[ "$script" != "mqtt" && "$script" != "socketio" && "$script" != "uwebsocket" && "$script" != "stomp" ]]; then
+if [[ "$script" != "mqtt" && "$script" != "socketio" && "$script" != "uwebsocket" ]]; then
     echo "Invalid script argument. Please provide 'mqtt', 'socketio', or 'uwebsocket'."
     exit 1
 fi
