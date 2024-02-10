4f10f861d95831f37e4429b0f2c08f32440ef608
Vinayak <vinayakj009@gmail.com>
2024-02-10 14:03:18 +0530

    core - added package and run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-scripts="uwebsocket socketio mqtt stomp expressWS"
+scripts="$1"
+if [ -z "$scripts" ]; then
+    scripts="socketio expressWS stomp mqtt uwebsocket bun elysia"
+fi
+# scripts="uwebsocket"
 
 print_with_border() {
     local input_string="$1"
@@ -25,14 +28,40 @@ testScript(){
     # We are running node 16 as uWebSocket does not work with latest node version
     # You can run `git apply latestNode.diff` and then run the test for other servers if you want to check them running on 
     # latest version of node
-    docker container run -d --rm -v $(pwd):/project -w /project --network serverTest --name $script node:19 npm run start $script
+    docker container run -d --rm -v $(pwd):/project -w /project --network serverTest --name $script oven/bun:1 bun src/server.ts server "$script"
 
     print_with_border "Storing server logs"
     mkdir -p logs
     docker container logs -f $script 2>&1 > logs/$script.log &
 
     print_with_border "Running client for $script"
-    docker container run -it --rm --name test-$script -v $(pwd):/project -w /project --network serverTest node:19 npm run test "$script"
+    docker container run -it --rm --name test-$script -v $(pwd):/project -w /project --network serverTest oven/bun:1 bun src/server.ts client "$script"
+    # docker container logs -f $script
+
+
+    print_with_border "Stopping server for $script"
+    docker container stop $script 2>&1 > /dev/null
+
+    print_with_border "Stopping requirements for $script"
+    bash stopRequirements.sh $script
+}
+
+testScriptuWs(){
+    print_with_border "Starting requirements for $script"
+    bash startRequirements.sh $script
+
+    print_with_border "Running server for $script"
+    # We are running node 16 as uWebSocket does not work with latest node version
+    # You can run `git apply latestNode.diff` and then run the test for other servers if you want to check them running on 
+    # latest version of node
+    docker container run -d --rm -v $(pwd):/project -w /project --network serverTest --name $script node:19 node src/uwebsocket.js server $script
+
+    print_with_border "Storing server logs"
+    mkdir -p logs
+    docker container logs -f $script 2>&1 > logs/$script.log &
+
+    print_with_border "Running client for $script"
+    docker container run -it --rm --name test-$script -v $(pwd):/project -w /project --network serverTest node:19 node src/uwebsocket.js client $script
     # docker container logs -f $script
 
 
@@ -54,16 +83,23 @@ print_with_border "Creating local network serverTest"
 docker network create serverTest 2>&1 > /dev/null
 
 
-print_with_border "Installing Node modules"
-docker container run -it --rm -v $(pwd):/project -w /project --network serverTest --name installer node:19 npm install
+# print_with_border "Installing Node modules"
+# docker container run -it --rm -v $(pwd):/project -w /project --network serverTest --name installer node:19 npm install
 
-print_with_border "Compiling typescript to js"
-docker container run -it --rm -v $(pwd):/project -w /project --network serverTest --name compiler node:19 npm run compile
+# print_with_border "Compiling typescript to js"
+# docker container run -it --rm -v $(pwd):/project -w /project --network serverTest --name compiler node:19 npm run compile
 # npm run compile
 
 rm "output.csv"
 for script in $scripts; do
-    testScript $script
+    if [ "$script" == "uwebsocket" ]; then
+        testScriptuWs $script
+    elif [ "$script" == "stomp" ]; then
+        echo "running stomp"
+        testScriptuWs $script
+    else
+        testScript $script
+    fi
     if [ ! -f "output.csv" ]; then
         head -n 1 "$script.csv" > output.csv
     fi
